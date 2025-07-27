0b689fde3927d8a29caa0be60cb6ce1c16dc8ddc
someear9h <tikotkarsamarth@gmail.com>
2025-07-27 18:48:26 +0530

    changed requirements.txt

--- frontend/src/util.ts
+++ frontend/src/util.ts
@@ -1,2 +1,3 @@
 
-export const API_BASE_URL = "/choreo-apis/readme-generator/backend/v1/api"
+// export const API_BASE_URL = "/choreo-apis/readme-generator/backend/v1/api"
+export const API_BASE_URL = "http://localhost:8000/api"

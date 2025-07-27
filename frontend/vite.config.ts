9c6a7d371691d783e0657942c60dcc82ff4abf03
someear9h <tikotkarsamarth@gmail.com>
2025-07-27 14:44:39 +0530

    removed hardcoing url from frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   console.log('VITE_DEBUG:', env.VITE_DEBUG)
+  console.log('COMMAND:', command) // <--- This uses 'command', so TS won't complain
 
   return {
     plugins: [react()],
     server: {
-      ...(env.VITE_DEBUG === 'true' && {
+      ...(command === 'serve' && env.VITE_DEBUG === 'true' && {
         proxy: {
           '/api': {
             target: 'http://localhost:8000',
